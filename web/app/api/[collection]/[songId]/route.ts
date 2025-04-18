5ed8f9280d0565d191ab2cf68a3def8335053013
Daniel <daniel@hegedu.es>
2025-04-18 11:50:16 +0200

    minimal poc to test if vercel can handle logic as edge function

---
+++ web/app/api/[collection]/[songId]/route.ts
@@ -0,0 +1,35 @@
+export const runtime = 'edge';
+
+interface Song {
+    id: string | number;
+    title: string;
+    artist: string;
+    year: string | number;
+}
+
+export async function GET(request: Request) {
+    const { searchParams, origin } = new URL(request.url);
+    const collection = searchParams.get('collection');
+    const id = searchParams.get('songId');
+
+    if (!collection || !id) {
+        return new Response('Missing collection or id', { status: 400 });
+    }
+
+    const response = await fetch(`${origin}/collection/${collection}.json`);
+    const data = await response.json();
+
+    if (!data) {
+        return new Response('Collection not found', { status: 404 });
+    }
+
+    const song = data.find((song: Song) => song.id == id);
+
+    if (!song) {
+        return new Response('Song not found', { status: 404 });
+    }
+
+    return new Response(JSON.stringify(song), {
+        headers: { 'Content-Type': 'application/json' },
+    });
+}
